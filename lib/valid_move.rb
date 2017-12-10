c880ff3112e6a4bba041827b379e40330a48022a
jpkim921 <jpkim921@gmail.com>
2017-12-10 19:17:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,9 +6,6 @@ else
     return false
   end
 
-  if idx.between(0, 8) == false
-      return false
-  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
